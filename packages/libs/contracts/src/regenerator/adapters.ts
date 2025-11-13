41c63c243f176a711c836343a1b649151c438bea
Theo Boutron <tboutron@lssm.co>
2025-11-13 05:26:31 +0100

    feat: add Regenerator service

---
+++ packages/libs/contracts/src/regenerator/adapters.ts
@@ -0,0 +1,37 @@
+import type {
+  BehaviorSignal,
+  ErrorSignal,
+  RegenerationContext,
+  TelemetrySignal,
+} from './types';
+
+export interface TelemetrySignalProvider {
+  pollTelemetry(
+    context: RegenerationContext,
+    since: Date,
+    until: Date
+  ): Promise<TelemetrySignal[]>;
+}
+
+export interface ErrorSignalProvider {
+  pollErrors(
+    context: RegenerationContext,
+    since: Date,
+    until: Date
+  ): Promise<ErrorSignal[]>;
+}
+
+export interface BehaviorSignalProvider {
+  pollBehavior(
+    context: RegenerationContext,
+    since: Date,
+    until: Date
+  ): Promise<BehaviorSignal[]>;
+}
+
+export interface SignalAdapters {
+  telemetry?: TelemetrySignalProvider;
+  errors?: ErrorSignalProvider;
+  behavior?: BehaviorSignalProvider;
+}
+
