c05161c05ed9be1eb50f7a666b73196c93b3c25a
Theo Boutron <tboutron@lssm.co>
2025-12-12 23:06:53 +0100

    feat: add examples of learning journey

--- packages/bundles/contractspec-studio/src/infrastructure/graphql/index.ts
+++ packages/bundles/contractspec-studio/src/infrastructure/graphql/index.ts
@@ -2,7 +2,6 @@ import { yoga } from '@elysiajs/graphql-yoga';
 import { Elysia } from 'elysia';
 import { schema } from './schema';
 import { createNextjsContext } from './context';
-import { exportContractsToGraphQLSchema } from '@lssm/app.api-contractspec/src/utils/graphql-schema-export';
 
 export function mountContractSpecStudioGraphQL(path = '/graphql') {
   const app = new Elysia();
@@ -16,8 +15,6 @@ export function mountContractSpecStudioGraphQL(path = '/graphql') {
 
     context: createNextjsContext,
   });
-
-  exportContractsToGraphQLSchema(schema, __dirname);
 
   return app.use(graphlYogaServer);
 }
