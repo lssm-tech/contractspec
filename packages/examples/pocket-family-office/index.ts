045130a71317a62c6782ce8c204a68ed1d0a4daf
Theo Boutron <tboutron@lssm.co>
2025-12-16 06:50:36 +0100

    feat: cli+vscode improvements

--- packages/examples/pocket-family-office/index.ts
+++
@@ -1,9 +0,0 @@
-export * from './blueprint';
-export * from './tenant.sample';
-export * from './connections/samples';
-export * from './knowledge/sources.sample';
-export * from './contracts';
-export * from './workflows';
-export * from './telemetry';
-export * from './pocket-family-office.feature';
-
