c9f80c080d3b54e3d299683c74aae50862d5f58c
Theo Boutron <tboutron@lssm.co>
2026-03-20 06:25:03 +0100

    chore: replace eslint+prettier by biomejs to optimize speed

--- packages/examples/minimal/src/handlers/user-input.handler.ts
+++ packages/examples/minimal/src/handlers/user-input.handler.ts
@@ -5,22 +5,22 @@ import { UserInputSpec } from '../contracts/user-input.contracts';
  * Handler for UserInput
  */
 export const userInputHandler: ContractHandler<typeof UserInputSpec> = async (
-	input,
-	context
+  input,
+  context
 ) => {
-	// TODO: Implement command logic
-
-	try {
-		// 1. Validate prerequisites
-		// 2. Perform business logic
-		// 3. Emit events if needed
-		// 4. Return result
-
-		return {
-			ok: true,
-		};
-	} catch (error) {
-		// Handle and map errors to spec.io.errors
-		throw error;
-	}
+  // TODO: Implement command logic
+  
+  try {
+    // 1. Validate prerequisites
+    // 2. Perform business logic
+    // 3. Emit events if needed
+    // 4. Return result
+    
+    return {
+      ok: true,
+    };
+  } catch (error) {
+    // Handle and map errors to spec.io.errors
+    throw error;
+  }
 };
